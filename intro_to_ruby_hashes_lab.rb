09974b1cacd1ef672ae2517a795a1781a1664b78
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 13:22:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,13 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+    hash = {}
+  hash[key] = value
+  return hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+ 
 end
 
 def update_counting_hash(hash, key)
